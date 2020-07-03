d6e693b47f10c6b0482d7432c4c0f27fb0e13b52
Mohit Chachra <chachramohit@gmail.com>
2020-07-03 13:06:30 +0530

    Updates

---
+++ precache-manifest.0f89bce704d4fdf40083a09dccf31eb3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52afd51204523ab687674c014c5e4239",
+    "url": "/Xylophone-react/index.html"
+  },
+  {
+    "revision": "089f539fe6d7810898e7",
+    "url": "/Xylophone-react/static/css/main.7d97be78.chunk.css"
+  },
+  {
+    "revision": "5c59034d5967e0d4eb82",
+    "url": "/Xylophone-react/static/js/2.8b1a72a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Xylophone-react/static/js/2.8b1a72a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "089f539fe6d7810898e7",
+    "url": "/Xylophone-react/static/js/main.112c68e2.chunk.js"
+  },
+  {
+    "revision": "0fe41e3c8a3113411826",
+    "url": "/Xylophone-react/static/js/runtime-main.3c0f079b.js"
+  },
+  {
+    "revision": "424eb0c2e7e4d3727dde48e8629bd577",
+    "url": "/Xylophone-react/static/media/a.424eb0c2.wav"
+  },
+  {
+    "revision": "b0963b9ba5a6904d115b9bb0acdfab4a",
+    "url": "/Xylophone-react/static/media/b.b0963b9b.wav"
+  },
+  {
+    "revision": "98ddcc4fffb6d64c9093c7caf8db978a",
+    "url": "/Xylophone-react/static/media/c.98ddcc4f.wav"
+  },
+  {
+    "revision": "8118569e1adba1d8c1c329ee8154334b",
+    "url": "/Xylophone-react/static/media/c2.8118569e.wav"
+  },
+  {
+    "revision": "90104779407bbb7549d19e6c5130bc3d",
+    "url": "/Xylophone-react/static/media/d1.90104779.wav"
+  },
+  {
+    "revision": "48ef30033d1fba34c76e9a2057370ca5",
+    "url": "/Xylophone-react/static/media/e1.48ef3003.wav"
+  },
+  {
+    "revision": "e3597a143e96296b4b8edd4e5be556f7",
+    "url": "/Xylophone-react/static/media/f.e3597a14.wav"
+  },
+  {
+    "revision": "433c4567c21d0a8f45769f9706d94d79",
+    "url": "/Xylophone-react/static/media/g.433c4567.wav"
+  }
+]);
